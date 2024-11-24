41eafa2c587e65d54a1097760484fc014846560c
Sergey <sergiy.sev@gmail.com>
2024-11-24 15:59:27 +0100

    Update main Navbar

--- lib/components/menu/Menu.tsx
+++ lib/components/menu/Menu.tsx
@@ -1,7 +1,19 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { Navbar, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
+import {
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  NavbarBrand,
+} from '@nextui-org/react';
+import { ChevronDownIcon, PlusIcon } from '@heroicons/react/16/solid';
 import clsx from 'clsx';
 
 const tagAStyles =
@@ -13,19 +25,69 @@ export default function Menu({ className }: { className?: string }) {
 
   const isActive = (path: string) => currentPath === path;
 
+  const dropdownMenuItems = [
+    {
+      key: 'pd',
+      href: '/pd',
+      label: 'პდ',
+      icon: PlusIcon,
+    },
+    {
+      key: 'kidney-assessment',
+      href: '/kidney-assessment',
+      label: 'შეფასება',
+      icon: PlusIcon,
+    },
+    {
+      key: 'infectious',
+      href: '/infectious',
+      label: 'ინფექციური',
+      icon: PlusIcon,
+    },
+    {
+      key: 'noninfectious',
+      href: '/noninfectious',
+      label: 'არაინფექციური',
+      icon: PlusIcon,
+    },
+  ];
+
   return (
     <Navbar className={clsx('', className)} position="static">
-      <NavbarItem isActive={isActive('/')}>
-        <Link href="/" aria-current={isActive('/') ? 'page' : undefined}>
-          მთავარი
-        </Link>
-      </NavbarItem>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {/* <NavbarItem isActive={isActive('/pds')}>
-          <Link color="foreground" href="/pds">
-            პდ
+      <NavbarBrand>
+        <NavbarItem isActive={isActive('/')}>
+          <Link href="/" aria-current={isActive('/') ? 'page' : undefined}>
+            მთავარი
           </Link>
-        </NavbarItem> */}
+        </NavbarItem>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <Dropdown className="shadow-md border rounded-lg">
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                endContent={<ChevronDownIcon className="w-4 h-4" />}
+                radius="sm"
+                variant="light"
+              >
+                ახალი...
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu aria-label="ახალი...">
+            {dropdownMenuItems.map(({ key, href, label, icon: Icon }) => (
+              <DropdownItem key={key} startContent={<Icon className="w-4 h-4" />} textValue={key}>
+                <Link href={href} className="text-sm text-black">
+                  {label}
+                </Link>
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+      <NavbarContent justify="end">
         <NavbarItem isActive={isActive('/exit')}>
           <Button href={`/exit`} as={Link} color="default" variant="ghost">
             გამოსვლა
