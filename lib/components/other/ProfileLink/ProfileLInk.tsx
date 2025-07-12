2837505dcf79f21f8a55d878b757b8c5ff5e0f68
Sergey Ermolaev <sergiy.sev@gmail.com>
2025-07-12 15:57:00 +0200

    Update ProfileLInk.tsx

--- lib/components/other/ProfileLink/ProfileLInk.tsx
+++ lib/components/other/ProfileLink/ProfileLInk.tsx
@@ -1,11 +1,14 @@
 import { UserCircleIcon } from '@heroicons/react/16/solid';
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
 export default function ProfileLink({ href }: { href: string }) {
+  const t = useTranslations();
+
   return (
     <Link href={href} className="inline-flex items-center gap-1 text-blue-900 hover:text-blue-600 mb-4">
       <UserCircleIcon className="w-4 h-4" />
-      <span>ფროფილი</span>
+      <span>{t('profile')}</span>
     </Link>
   );
 }
